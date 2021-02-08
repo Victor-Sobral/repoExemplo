eef5567b1d56439a3cd4d7728c332fede03217ec
Victor Sobral <a.victor.sobral@gmail.com>
2021-02-08 20:58:31 -0300

    primeira versão

---
+++ Calculadora.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidade;
+
+/**
+ *
+ * @author victorsobral
+ */
+public class Calculadora implements Math {
+    
+    @Override
+    
+    public double add(double a, double b){
+        return 0;
+    }
+
+    @Override
+    
+    public double sub(double a, double b){
+        return 0;
+    }    
+        @Override
+    
+    public double mult(double a, double b){
+        return 0;
+    }
+    
+        @Override
+    
+    public double div(double a, double b){
+        return 0;
+    }
+}
